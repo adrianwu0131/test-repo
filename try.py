14e5a1234fb7b4abd0c9ffbe7431626410656bb2
adrianwu <adrianwu0131@gmail.com>
2016-05-13 00:16:51 +0800

    for rebase test

--- try.py
+++ try.py
@@ -3,4 +3,8 @@ for i in range(1,10):
 	print (i)
 #add aline
 #add another line
+<<<<<<< a62922bfd2717051c39749e8aac1eaa165767c9a
 #after cat branch add one line
+=======
+#add line for cat
+>>>>>>> for branch test`
